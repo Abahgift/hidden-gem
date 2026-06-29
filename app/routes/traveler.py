b715eb24fbb3b9e39d2bc21beb49cc22825dc303
Gift Abah <abahgift49@gmail.com>
2026-06-29 16:24:14 +0100

    Flask Application Structure Setup and Home Route Implementation

--- app/routes/traveler.py
+++ app/routes/traveler.py
@@ -1,6 +1,25 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
+@app.route("/")
 def landing_page():
     return render_template("traveler/homePage.html")
+
+@app.route("/signup/")
+def signup():
+    return render_template("traveler/signUp.html")
+
+
+@app.route("/login/")
+def login():
+    return render_template("traveler/login.html")
+
+
+@app.route("/otp/", methods=["GET","POST"])
+def otp():
+    return render_template("traveler/otp.html")
+
+
+@app.route("/dashboard/", methods=["GET", "POST"])
+def dashboard():
+    return render_template("traveler/dashboard.html")
